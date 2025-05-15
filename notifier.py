5be6c31eb80c7ca81ac1e3cc00d96315a9d42666
5252n <76904613+5252n@users.noreply.github.com>
2025-05-15 12:09:55 +0300

    notifier.py

--- notifier.py
+++ notifier.py
@@ -1,7 +1,6 @@
 import requests
 
-TOKEN = "7366831152:AAF5Ax-OWmIvwEO2PsKnAFsyfTRw_aY-fqc"
-CHAT_ID = "25166548"
+
 
 def send_telegram(msg):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
